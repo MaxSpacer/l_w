408e81f4264f59f77e03d42bc47a1ae16e4507d1
MaxSpacer <max_spacer@mail.ru>
2019-02-08 04:14:08 +0300

    +2

--- subscribers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from subscribers.models import *
-
-# Register your models here.
-
-
-class SubscribersAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Subscribers._meta.fields]
-admin.site.register(Subscribers, SubscribersAdmin)
