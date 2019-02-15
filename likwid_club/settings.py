9d43d533364184e88f9edcb61c8ccf0e3dbc2806
MaxSpacer <max_spacer@mail.ru>
2019-02-15 17:54:44 +0300

    40

--- likwid_club/settings.py
+++ likwid_club/settings.py
@@ -181,14 +181,6 @@ TINYMCE_DEFAULT_CONFIG = {
     'statusbar': True,
     }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'localhost'
-# EMAIL_PORT = '1025'
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "info@likwid.club"
-EMAIL_HOST_PASSWORD = "Il123456!"
-EMAIL_USE_SSL = True
 #uncomment
 try:
     from .settings_prod import *
