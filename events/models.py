473056007bd06b4a14afbab038524d1c55f39b3d
MaxSpacer <max_spacer@mail.ru>
2019-02-19 02:32:42 +0300

    start event reformat

--- events/models.py
+++ events/models.py
@@ -23,7 +23,7 @@ class EventCategory(models.Model):
 
 class Event(models.Model):
 	name = models.CharField(max_length=64, blank=True, null=True, default=None)
-	image = models.ImageField(upload_to='event_images/', null=True, default=None)
+	# image = models.ImageField(upload_to='event_images/', null=True, default=None)
 	category = models.ForeignKey(EventCategory, on_delete=models.SET_DEFAULT, max_length=64, blank=True, null=True, default=None)
 	description = models.TextField(max_length=256,blank=True, null=True, default=None)
 	content = HTMLField('Content', null=True, default=None)
@@ -77,6 +77,24 @@ class EventJoiner(models.Model):
         verbose_name_plural = 'Подписавшиеся на мероприятия'
 
 
-from .signals import send_mail_join_event
+class EventImage(models.Model):
+	event = models.ForeignKey(Event, on_delete=models.SET_DEFAULT, blank=True, null=True, default=None)
+	image = models.ImageField(upload_to='product_images/')
+	is_main = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+	created = models.DateTimeField(auto_now_add=True , auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False , auto_now=True)
 
+	def __str__(self):
+		return "%s" % self.image
+
+	class Meta:
+		verbose_name = 'Фотография продукции'
+		verbose_name_plural = 'Фотографии продукции'
+
+
+
+
+
+from .signals import send_mail_join_event
 post_save.connect(send_mail_join_event,sender=EventJoiner,dispatch_uid="my_unique_identifier")
