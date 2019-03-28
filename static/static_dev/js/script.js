21383d9e46dcd5af3b3f55a2110d3fddbefa1599
макс <max_spacer@mail.ru>
2019-03-28 03:35:29 +0300

    test

--- static/static_dev/js/script.js
+++ static/static_dev/js/script.js
@@ -11,7 +11,38 @@
 //         $(this).addClass('active');
 //     });
 
+jQuery(function ($) {
+    function fix_size() {
+        var images = $('.img-container img');
+        images.each(setsize);
 
+        function setsize() {
+            var img = $(this),
+                img_dom = img.get(0),
+                container = img.parents('.img-container');
+            if (img_dom.complete) {
+                resize();
+            } else img.one('load', resize);
+
+            function resize() {
+                if ((container.width() / container.height()) > (img_dom.width / img_dom.height)) {
+                    img.width('100%');
+                    img.height('auto');
+                } else {
+                    img.height('100%');
+                    img.width('auto');
+                }
+                var marginx=(img.width()-container.width())/-2,
+                    marginy=(img.height()-container.height())/-2;
+               console.log(marginx);
+               img.css({'margin-left': marginx, 'margin-top': marginy});
+
+            }
+        }
+    }
+    $(window).on('resize', fix_size);
+    fix_size();
+});
 $(document).ready(function() {
 
     var form = $('.formBuyProduct');
